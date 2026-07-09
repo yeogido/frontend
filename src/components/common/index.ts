4e5477809eac318cd80ba81c42d4cdb1497c9f43
kimtaehyeokkkk <113419813+kimtaehyeokkkk@users.noreply.github.com>
2026-07-10 00:00:00 +0900

    [FEAT] Home ReviewSection 및 ReviewCard 구현 (#18)
    * feat: Home ReviewSection 및 ReviewCard 구현
    
    * refactor: improve ReviewCard accessibility

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,3 +4,5 @@ export { default as ContentCard } from './ContentCard';
 export { default as ContentCardSkeleton } from './ContentCardSkeleton';
 export { default as CourseCard } from './CourseCard';
 export { default as CourseCardSkeleton } from './CourseCardSkeleton';
+export { default as ReviewCard } from './ReviewCard';
+export { default as ReviewCardSkeleton } from './ReviewCardSkeleton';
