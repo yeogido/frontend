76b4bd1680d42d368b6caec60a945159fb10506c
kimtaehyeokkkk <113419813+kimtaehyeokkkk@users.noreply.github.com>
2026-07-09 22:14:24 +0900

    [FEAT] Home CourseSection UI 구현
    * feat: Home CourseSection UI 구현
    
    * chore: CodeRabbit 설정 추가
    
    * refactor: apply CodeRabbit review feedback

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,5 @@ export { default as Logo } from './Logo';
 export { default as SectionHeader } from './SectionHeader';
 export { default as ContentCard } from './ContentCard';
 export { default as ContentCardSkeleton } from './ContentCardSkeleton';
+export { default as CourseCard } from './CourseCard';
+export { default as CourseCardSkeleton } from './CourseCardSkeleton';
