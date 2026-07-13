7e03833e8b808a1fca3487f1d43d964f6f83e7b0
unknown <tateth2009@naver.com>
2026-07-13 12:28:46 +0900

    refactor: 지도 확대/축소 인터랙션 개선

--- src/pages/home/map/components/MapViewport.tsx
+++ src/pages/home/map/components/MapViewport.tsx
@@ -4,7 +4,7 @@ import PolygonLayer from './PolygonLayer';
 
 const MapViewport = forwardRef<SVGGElement>(function MapViewport(_, ref) {
   return (
-    <g ref={ref} id="map-content">
+    <g ref={ref}>
       <PolygonLayer />
     </g>
   );
