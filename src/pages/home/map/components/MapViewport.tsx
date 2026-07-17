0b5da6bd08b490568771b5b7af6fff4e4f1f4378
kimtaehyeokkkk <113419813+kimtaehyeokkkk@users.noreply.github.com>
2026-07-18 03:29:09 +0900

    [STYLE] 서울 라벨 및 지도 라벨 가독성 개선
    * style: 서울 라벨 위치 및 오프셋 적용
    
    * style: improve map label collision handling

--- src/pages/home/map/components/MapViewport.tsx
+++ src/pages/home/map/components/MapViewport.tsx
@@ -6,17 +6,18 @@ import ProvinceLayer from './ProvinceLayer';
 
 interface MapViewportProps {
   zoomLevel: number;
+  renderScale: number;
 }
 
 const MapViewport = forwardRef<SVGGElement, MapViewportProps>(
-  function MapViewport({ zoomLevel }, ref) {
+  function MapViewport({ zoomLevel, renderScale }, ref) {
     return (
       <g ref={ref} id="map-content">
         <ProvinceLayer />
 
         <CityLayer zoomLevel={zoomLevel} />
 
-        <LabelLayer zoomLevel={zoomLevel} />
+        <LabelLayer zoomLevel={zoomLevel} renderScale={renderScale} />
       </g>
     );
   },
--- src/pages/home/map/constants/map.ts
+++ src/pages/home/map/constants/map.ts
@@ -6,3 +6,8 @@ export const MAX_ZOOM = 5;
 export const ZOOM_STEP = 0.5;
 
 export const CITY_LAYER_ZOOM = 3;
+
+export const BASE_ZOOM_SCALE = 2.5;
+
+export const PROVINCE_LABEL_FONT_SIZE = 20; // 화면상 목표 크기(px 느낌의 스크린 기준 값)
+export const CITY_LABEL_FONT_SIZE = 12;
