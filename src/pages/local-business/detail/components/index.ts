a308eebe3dcc5b1c9b303276de7ac568a4877ae3
yoonsunmindd <0701sally@gmail.com>
2026-07-30 01:52:57 +0900

    fix: local-business detail 중복 폴더 삭제 (merge 과정에서 실수로 복원됨, 실제 라우트는 pages/detail/local-business 참조) (#156)
    Co-authored-by: 윤선민 <yoonsunmin@yunseonmin-ui-MacBookAir.local>

--- src/pages/local-business/detail/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as DetailHeroImage } from './DetailHeroImage';
-export { default as DetailTitleSection } from './DetailTitleSection';
-export { default as DetailIntroCard } from './DetailIntroCard';
-export { default as DetailInfoCard } from './DetailInfoCard';
-export { default as DetailMapPlaceholder } from './DetailMapPlaceholder';
-export { default as DetailRelatedPostCard } from './DetailRelatedPostCard';
-export { default as DetailAuthorCard } from './DetailAuthorCard';
